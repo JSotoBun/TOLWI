04920c24323bc67d6a45b35049fa65f8639168b3
JSotoBun <BunSoto88@gmail.com>
2020-04-28 13:16:27 +1200

    Adding Custom Game Instance

---
+++ Source/TOLWI/TOLWIGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TOLWIGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOLWI_API UTOLWIGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
